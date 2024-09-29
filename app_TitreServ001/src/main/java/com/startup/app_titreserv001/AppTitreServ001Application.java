b06c5d9b90ca5325e20067c56db6adc4fe547318
drallas123 <s.allard789@gmail.com>
2024-09-29 16:35:18 +0200

    Initial commit

---
+++ app_TitreServ001/src/main/java/com/startup/app_titreserv001/AppTitreServ001Application.java
@@ -0,0 +1,13 @@
+package com.startup.app_titreserv001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTitreServ001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppTitreServ001Application.class, args);
+    }
+
+}
---
+++ app_TitreServ001/src/test/java/com/startup/app_titreserv001/AppTitreServ001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startup.app_titreserv001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTitreServ001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
